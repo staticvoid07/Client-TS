6bc8029bb2e019594f27487d2d0ae511242d242e
staticvoid07 <static.v0id2000@gmail.com>
2025-12-08 22:06:15 -0300

    lost of changes

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -2071,10 +2071,10 @@ export class Client extends GameShell {
 
             if (now - this.idleCycle > 90_000) {
                 // no input in 90s, notify the server
-                this.pendingLogout = 250;
-                this.idleCycle += 10_000; // 10s backoff
+                //this.pendingLogout = 250;
+                //this.idleCycle += 10_000; // 10s backoff
 
-                this.out.pIsaac(ClientProt.IDLE_TIMER);
+                //this.out.pIsaac(ClientProt.IDLE_TIMER);
             }
 
             this.macroCameraCycle++;
